7440abf1e16c5f758090dbce60f730977b8a5c86
rohithDhoodhipala <rohith.dhoodhipala@octanner.com>
2024-09-19 20:30:35 +0530

    Update HomeBody.tsx

--- src/components/Home/HomeBody.tsx
+++ src/components/Home/HomeBody.tsx
@@ -10,7 +10,7 @@ const HomeBody = () => {
         <p className={styles.helloTitle} >
           <span className={`${styles.blink} ${styles.fontGreen}`} style={{marginRight: '5px'}}>&#60;</span>HELLO<span className={`${styles.blink} ${styles.fontGreen}`}>/&#62;</span>
         </p>
-        <p className={styles.homeDescription}>Me, <span className={`${styles.fontGreen} ${styles.standOutFont}`}>ROHITH REDDY</span> currently working as a Software Engineer at <a className={`${styles.fontGreen} ${styles.standOutFont}`} href="https://www.octanner.com/in/" target="_blank" rel="noreferrer">O.C. Tanner</a>. </p>
+        <p className={styles.homeDescription}>Me, <span className={`${styles.fontGreen} ${styles.standOutFont}`}>ROHITH REDDY</span> currently working as a Software Engineer at <a className={`${styles.fontGreen} ${styles.standOutFont}`} href="https://www.octanner.com" target="_blank" rel="noreferrer">O.C. Tanner</a>. </p>
       </div>
       <img className={styles.profileImage} src={myImage} alt={`Rohith's pic`} />
     </div>
